48b868cc66d35d433d07d2e5932402b7af4996c6
Amy Phung <amyngph@gmail.com>
2020-06-11 16:58:26 -0700

    Rename project to joint_calibration, create bringup and starting source file

--- src/calibrate.cpp
+++ src/calibrate.cpp
@@ -1,90 +1,59 @@
-//
-// Created by amy on 6/9/20.
-//
-
-#include "ros/ros.h"
-#include "std_msgs/String.h"
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <sensor_msgs/JointState.h>
+#include <apriltag_msgs/ApriltagArrayStamped.h>
+#include <std_msgs/String.h>
 
 #include <sstream>
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
-int main(int argc, char **argv)
+using namespace sensor_msgs;
+using namespace apriltag_msgs;
+//using namespace message_filters;
+
+void jsCallback(const JointState::ConstPtr& msg)
 {
-    /**
-     * The ros::init() function needs to see argc and argv so that it can perform
-     * any ROS arguments and name remapping that were provided at the command line.
-     * For programmatic remappings you can use a different version of init() which takes
-     * remappings directly, but for most command-line programs, passing argc and argv is
-     * the easiest way to do it.  The third argument to init() is the name of the node.
-     *
-     * You must call one of the versions of ros::init() before using any other
-     * part of the ROS system.
-     */
-    ros::init(argc, argv, "talker");
+    ROS_INFO("asdfasfd");
+}
 
+void camCallback(const ApriltagArrayStamped::ConstPtr& msg)
+{
+    ROS_INFO("2222");
+}
 
-    /**
-     * NodeHandle is the main access point to communications with the ROS system.
-     * The first NodeHandle constructed will fully initialize this node, and the last
-     * NodeHandle destructed will close down the node.
-     */
-    ros::NodeHandle n;
+void callback(const JointState::ConstPtr& joint_states, const ApriltagArrayStamped::ConstPtr& tags)
+{
+    ROS_INFO("aaa");
+    // Solve all of perception here...
+    std_msgs::String msg;
 
-    /**
-     * The advertise() function is how you tell ROS that you want to
-     * publish on a given topic name. This invokes a call to the ROS
-     * master node, which keeps a registry of who is publishing and who
-     * is subscribing. After this advertise() call is made, the master
-     * node will notify anyone who is trying to subscribe to this topic name,
-     * and they will in turn negotiate a peer-to-peer connection with this
-     * node.  advertise() returns a Publisher object which allows you to
-     * publish messages on that topic through a call to publish().  Once
-     * all copies of the returned Publisher object are destroyed, the topic
-     * will be automatically unadvertised.
-     *
-     * The second parameter to advertise() is the size of the message queue
-     * used for publishing messages.  If messages are published more quickly
-     * than we can send them, the number here specifies how many messages to
-     * buffer up before throwing some away.
-     */
-    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+    std::stringstream ss;
+    ss << "hello world ";
+    msg.data = ss.str();
 
-    ros::Rate loop_rate(10);
+    ROS_INFO("%s", msg.data.c_str());
 
-    /**
-     * A count of how many messages we have sent. This is used to create
-     * a unique string for each message.
-     */
-    int count = 0;
-    while (ros::ok())
-    {
-        /**
-         * This is a message object. You stuff it with data, and then publish it.
-         */
-        std_msgs::String msg;
+}
 
-        std::stringstream ss;
-        ss << "hello world " << count;
-        msg.data = ss.str();
+int main(int argc, char** argv)
+{
+    ROS_INFO("hiya");
+    ros::init(argc, argv, "calibration_node");
+    ros::NodeHandle nh;
 
-        ROS_INFO("%s", msg.data.c_str());
+    ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 1000);
 
-        /**
-         * The publish() function is how you send messages. The parameter
-         * is the message object. The type of this object must agree with the type
-         * given as a template parameter to the advertise<>() call, as was done
-         * in the constructor above.
-         */
-        chatter_pub.publish(msg);
+//    ros::Subscriber sub = nh.subscribe("/joint_states", 1000, jsCallback);
+//    ros::Subscriber sub2 = nh.subscribe("/camera/fisheye/tags", 1000, camCallback);
+    message_filters::Subscriber<JointState> joint_sub(nh, "/joint_states", 1);
+    message_filters::Subscriber<ApriltagArrayStamped> tag_sub(nh, "/camera/fisheye/tags", 1);
 
-        ros::spinOnce();
+    typedef message_filters::sync_policies::ApproximateTime<JointState, ApriltagArrayStamped> MySyncPolicy;
+    message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), joint_sub, tag_sub);
 
-        loop_rate.sleep();
-        ++count;
-    }
+    sync.registerCallback(boost::bind(&callback, _1, _2));
 
+    ros::spin();
 
     return 0;
 }
