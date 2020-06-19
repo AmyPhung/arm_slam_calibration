3a2d8285dfa7ec45a677584302bb83cb00f0a5eb
Amy Phung <amyngph@gmail.com>
2020-06-19 11:50:49 -0700

    Update calibration data collector

--- scripts/data_formatter.py
+++ scripts/data_formatter.py
@@ -35,19 +35,24 @@ class DataFormatter():
         self.calibration_pub = rospy.Publisher("/calibration_data",
             CalibrationData, queue_size=10)
 
-        # TODO: Remove hardcode
-        self.tags = ['virtual_tag_584', 'virtual_tag_582', 'virtual_tag_580',
-                     'virtual_tag_579', 'virtual_tag_578', 'virtual_tag_577',
-                     'virtual_tag_9', 'virtual_tag_5', 'virtual_tag_7']
+        self.tags = rospy.get_param("tag_frames",
+            ['tag_584', 'tag_582', 'tag_580',
+             'tag_579', 'tag_578', 'tag_577',
+             'tag_9', 'tag_5', 'tag_7'])
+        self.calib_filename = rospy.get_param("calib_filename",
+            '/home/amy/whoi_ws/src/joint_calibration/bags/calibration_data.bag')
 
+        self.base_tag_frames = \
+            [i + j for i, j in zip(['base_']*len(self.tags), self.tags)]
+        self.virtual_tag_frames = \
+            [i + j for i, j in zip(['virtual_']*len(self.tags), self.tags)]
         self.blank = [None] * len(self.tags)
 
-        self.base_msgs = OrderedDict(zip(self.tags, self.blank))
-        self.fisheye_msgs = OrderedDict(zip(self.tags, self.blank))
+        self.base_msgs = OrderedDict(zip(self.base_tag_frames, self.blank))
+        self.fisheye_msgs = OrderedDict(zip(self.virtual_tag_frames, self.blank))
         self.state_msg = None
 
-        # TODO: Make this a param
-        self.bag = rosbag.Bag('/home/amy/whoi_ws/src/joint_calibration/bags/calibration_data.bag', 'w') # TODO: make this a param
+        self.bag = rosbag.Bag(self.calib_filename, 'w')
 
         # write the URDF
         description = String()
@@ -80,8 +85,8 @@ class DataFormatter():
         data_msg = CalibrationData()
         data_msg.joint_states = self.state_msg
 
-        # TODO: Put this somewhere else - just for quick testing
-        data_msg.joint_states.position = list(map(lambda x : x - 1, data_msg.joint_states.position))
+        # TODO: Put this somewhere else - just for quick testing of offsets
+        # data_msg.joint_states.position = list(map(lambda x : x - 1, data_msg.joint_states.position))
 
         fisheye_obs = Observation()
         fisheye_obs.sensor_name = "fisheye"
