8588c020830ae4be16a8cb6fc45af93e8c7fc0cb
Amy Phung <amyngph@gmail.com>
2020-06-25 08:20:22 -0700

    Clean up chain forward kinematics, add comments

--- include/joint_calibration/ChainModel.h
+++ include/joint_calibration/ChainModel.h
@@ -22,10 +22,24 @@ namespace joint_calibration {
         ChainModel(const std::string& robot_description,
                 std::string root, std::string tip);
         virtual ~ChainModel();
+
+        /**
+          *  \brief Compute point position along kinematic chain to project
+          *  measurement taken in tip frame in root frame
+          *  \param param_manager The parameter manager
+          *  \param input_pts All of the observations of the same point
+          *  \param output_pts The transformed observations written to a pointcloud
+          */
         void project(joint_calibration::ParameterManager& param_manager,
                 const joint_calibration::PointGroup& input_pts,
                 sensor_msgs::PointCloud& output_pts);
 
+        /**
+          *  \brief Compute forward kinematics to get frame that transforms
+          *  points to root frame
+          *  \param param_manager The parameter manager
+          *  \param state The joint states at the time of measurement
+          */
         KDL::Frame getChainFK(joint_calibration::ParameterManager& param_manager,
                 const sensor_msgs::JointState& state);
 
