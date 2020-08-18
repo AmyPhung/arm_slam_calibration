4f86f8483ddfd05b2864bd91bd22f66f3c865a47
Amy Phung <amyngph@gmail.com>
2020-08-18 10:00:54 -0700

    Update visualziation scripts

--- min_variance_calibration/scripts/visualization/sweep_joint_errors.py
+++ min_variance_calibration/scripts/visualization/sweep_joint_errors.py
@@ -23,6 +23,12 @@ import time
 import copy
 import pandas as pd
 
+# Help find custom libraries
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
 # Custom Libraries
 import calibration_bridge as bridge
 import helper_functions as hf
--- min_variance_calibration/scripts/visualization/sweep_noise_levels.py
+++ min_variance_calibration/scripts/visualization/sweep_noise_levels.py
@@ -23,6 +23,12 @@ import yaml
 import time
 import copy
 
+# Help find custom libraries
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
 # Custom Libraries
 import calibration_bridge as bridge
 import helper_functions as hf
